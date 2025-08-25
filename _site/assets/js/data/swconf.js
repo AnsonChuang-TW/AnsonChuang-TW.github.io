b91b9fc6fef4fd88646befdb6825ab9082f590b7
AnsonChuang-TW <ansonchuangtw@gmail.com>
2025-08-25 13:46:46 +0800

    Auto commit: 2025/08/25 週一 13:46:46.45

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756100188',resources: [
+    cacheName: 'chirpy-1756100778',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
