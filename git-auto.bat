94f6cc22638ccc87bbb71751ecedc73ae9db34a0
AnsonChuang-TW <ansonchuangtw@gmail.com>
2025-08-25 15:23:25 +0800

    Auto commit: 2025/08/25 週一 15:23:25.73

--- git-auto.bat
+++ git-auto.bat
@@ -2,14 +2,46 @@
 echo Git Auto Script
 echo.
 
+:: Sync images from _posts to assets/img
+echo Syncing images from _posts to assets/img...
+if exist "_posts\assets\img\image" (
+    if not exist "assets\img\image" mkdir "assets\img\image"
+    
+    :: Copy images from _posts to assets/img
+    for %%f in ("_posts\assets\img\image\*.*") do (
+        if exist "assets\img\image\%%~nxf" (
+            echo Copying: %%~nxf
+        ) else (
+            echo New image: %%~nxf
+        )
+        copy "%%f" "assets\img\image\" >nul
+    )
+    
+    :: Remove images from assets/img that don't exist in _posts
+    for %%f in ("assets\img\image\*.*") do (
+        if not exist "_posts\assets\img\image\%%~nxf" (
+            echo Removing: %%~nxf
+            del "%%f"
+        )
+    )
+    
+    echo Images synced successfully!
+) else (
+    echo No _posts\assets\img\image folder found.
+)
+echo.
+
 :: Add all changes
+echo Adding all changes...
 git add .
 
 :: Commit with timestamp
 set timestamp=%date% %time%
+echo Committing changes...
 git commit -m "Auto commit: %timestamp%"
 
 :: Push to remote
+echo Pushing to remote...
 git push
 
 echo Done!
